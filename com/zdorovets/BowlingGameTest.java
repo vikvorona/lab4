988cb374638c8e4abe4da71256caea77c05aa53f
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-10-21 15:08:45 +0300

    v1.4.1
    заготовка для будущего метода rollMany

--- com/zdorovets/BowlingGameTest.java
+++ com/zdorovets/BowlingGameTest.java
@@ -29,8 +29,10 @@ public class BowlingGameTest extends TestCase {
 	
 	@Test
 	public void testGutterGame() throws Exception {
-	    for (int i=0; i<20; i++)
-	    	g.roll(0);
+		int n = 20;
+		int pins = 0;
+	    for (int i=0; i<n; i++)
+	    	g.roll(pins);
 	    Assert.assertEquals(0, g.score());
 	}
 	@Test
