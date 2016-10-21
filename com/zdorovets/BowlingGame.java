7a9ea6e74d0b03e5d208cedcf17f7f885a0215e5
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-10-21 14:54:52 +0300

    v1.4
    добавлен метод setUp. Однако метод roll вызывается по-прежнему много раз

--- com/zdorovets/BowlingGame.java
+++ com/zdorovets/BowlingGame.java
@@ -5,15 +5,17 @@ package com.zdorovets;
  * created on 21/10/16
  */
 public class BowlingGame {
+	private int score = 0;
 	/*
 	 * @param pins - количество кеглей
 	 */
 	public void roll(int pins) {
+		score += pins;
 	}
 	/* 
 	 * @return количество очков
 	 */
 	public int score(){
-		return 0;
+		return score;
 	}
 }
--- com/zdorovets/BowlingGameTest.java
+++ com/zdorovets/BowlingGameTest.java
@@ -1,24 +1,42 @@
 package com.zdorovets;
+import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
+/*
+ * MyException class
+ * @author Evgeny Zdorovets
+ * created on 21/10/16
+ */
+class MyException extends Exception {
+    private static final long serialVersionUID = 1L;
+    public MyException(String message) {
+        super(message);
+    }
+}
 /*
  * Unit tests for BowlingGame
  * @author Evgeny Zdorovets
  * created on 21/10/16
  */
-public class BowlingGameTest {
+public class BowlingGameTest extends TestCase {
+	private BowlingGame g;
+	/*
+	 * создает игру
+	 */
+	protected void setUp() throws Exception {
+		g = new BowlingGame();
+	}
+	
 	@Test
 	public void testGutterGame() throws Exception {
-		BowlingGame g = new BowlingGame();
 	    for (int i=0; i<20; i++)
-	      g.roll(0);
+	    	g.roll(0);
 	    Assert.assertEquals(0, g.score());
 	}
 	@Test
 	public void testAllOnes() throws Exception {
-		BowlingGame g = new BowlingGame();
 	    for (int i=0; i<20; i++)
-	      g.roll(1);
+	    	g.roll(1);
 	    Assert.assertEquals(20, g.score());
 	}
 }
