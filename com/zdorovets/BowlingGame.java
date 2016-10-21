b749f4655b9cb03a9fd25da2e521f58bb8a0e008
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-10-21 12:58:29 +0300

    v1.0
    созданы класс игры в боулинг и класс для JUnit-тестов

---
+++ com/zdorovets/BowlingGame.java
@@ -0,0 +1,9 @@
+package com.zdorovets;
+/*
+ * BowlingGame class
+ * @author Evgeny Zdorovets
+ * created on 21/10/16
+ */
+public class BowlingGame {
+
+}
---
+++ com/zdorovets/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package com.zdorovets;
+import org.junit.Test;
+/*
+ * Unit tests for BowlingGame
+ * @author Evgeny Zdorovets
+ * created on 21/10/16
+ */
+public class BowlingGameTest {
+	@Test
+	  public void testGutterGame() throws Exception {
+	    BowlingGame g = new BowlingGame();
+	  }
+}
