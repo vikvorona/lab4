f5a31d3a3e2a6052ec6b41a554959f2a1167b62c
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-10-22 15:50:01 +0300

    v1.7.3
    Выделен метод isStrike

--- com/zdorovets/BowlingGame.java
+++ com/zdorovets/BowlingGame.java
@@ -20,7 +20,7 @@ public class BowlingGame {
 		int score = 0;
 		int frameIndex = 0;
 		for (int frame = 0; frame < 10; frame++){
-			if(rolls[frameIndex] == 10){ // strike			
+			if(isStrike(frameIndex)){		
 				score += 10 + strikeBonus(frameIndex);
 				frameIndex++;
 			}
@@ -40,6 +40,13 @@ public class BowlingGame {
 	 */
 	private boolean isSpare(int frameIndex){
 		return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+	}
+	/*
+     * @param frameIndex индекс захода
+     * @return ситуация strike или нет
+     */
+	private boolean isStrike(int frameIndex){
+		return rolls[frameIndex] == 10;
 	}
 	/* 
 	 * @param frameIndex индекс захода
