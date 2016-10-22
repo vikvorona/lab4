c57e8b17e85b3b0a98dbbdd1b0a526d86a81ee33
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-10-22 14:24:18 +0300

    v1.6
    прописан случай score - когда сумма сбитых кеглей в непосредственно двух
    попытках равна 10. Третий тест успешен.

--- com/zdorovets/BowlingGame.java
+++ com/zdorovets/BowlingGame.java
@@ -19,9 +19,14 @@ public class BowlingGame {
 	public int score(){
 		int score = 0;
 		int i = 0;
-		for(int frame = 0; frame < 10; frame++){
-			score += rolls[i] + rolls[i+1];
-			i += 2;
+		for (int frame = 0; frame < 10; frame++){
+			if (rolls[i] + rolls[i + 1] == 10){
+				score += 10 + rolls[i + 2];
+				i += 2;
+			} else {
+				score += rolls[i] + rolls[i+1];
+				i += 2;
+			}
 		}
 		return score;
 	}
