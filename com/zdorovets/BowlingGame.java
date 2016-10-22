1e552583ad790ee44d1093e5026742175bed5ef9
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-10-22 15:17:52 +0300

    v1.7.1
    добавлена проверка на страйк без выделения отдельных методов

--- com/zdorovets/BowlingGame.java
+++ com/zdorovets/BowlingGame.java
@@ -20,7 +20,12 @@ public class BowlingGame {
 		int score = 0;
 		int frameIndex = 0;
 		for (int frame = 0; frame < 10; frame++){
-			if (isSpare(frameIndex)){
+			if(rolls[frameIndex] == 10) // strike
+			{
+				score += 10 + rolls[frameIndex + 1]+ rolls[frameIndex + 2];
+				frameIndex++;
+			}
+			else if (isSpare(frameIndex)){
 				score += 10 + rolls[frameIndex + 2];
 				frameIndex += 2;
 			} else {
