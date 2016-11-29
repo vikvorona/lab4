ba7f2c9d6aca8f2e5f73ea680b82a7d88c027438
Alexandra Grigorova <iskandera95@mail.ru>
2016-11-29 19:09:49 +0300

    v0.2 added two methods: for getting score and for rolling. added testcase.

--- src/com/etu/grigorova/ScoringBowling.java
+++ src/com/etu/grigorova/ScoringBowling.java
@@ -4,4 +4,15 @@ package com.etu.grigorova;
  * Created by zikovam on 29.11.16.
  */
 public class ScoringBowling {
+    //our bowling score
+    private int score = -1;
+
+    public void roll (int pins){
+
+    }
+
+    public int getScore () {
+        return score;
+    }
+
 }
--- test/com/etu/grigorova/ScoringBowlingTest.java
+++ test/com/etu/grigorova/ScoringBowlingTest.java
@@ -1,8 +1,17 @@
 package com.etu.grigorova;
+import org.junit.Test;
+import org.junit.Assert;
 
 /**
  * Created by zikovam on 29.11.16.
  */
 public class ScoringBowlingTest {
-    ScoringBowling bowling;
+    @Test
+    public void testGame() throws BowlingGameException{
+    ScoringBowling bowling = new ScoringBowling();
+        for (int i = -1; i < 15; i++) {
+            bowling.roll(0);
+        }
+        Assert.assertEquals(0,bowling.getScore());
+    }
 }
