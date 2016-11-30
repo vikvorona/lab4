a254ce9c819da8e9a274a4de4c60848b4e461ca4
Daniil Chernyshov <chernyshov777@gmail.com>
2016-11-30 19:47:06 +0300

    add test for simple roll without strike and spare

--- src/test/java/com/chernyshov777/FrameTest.java
+++ src/test/java/com/chernyshov777/FrameTest.java
@@ -18,9 +18,26 @@ public class FrameTest {
 
     @Test
     public void testConstructor() {
-        Assert.assertEquals("wrong current roll", 0, frame.getCurrentRoll());
+        Assert.assertEquals("wrong current roll", 1, frame.getCurrentRoll());
         Assert.assertEquals("wrong first Roll score", 0, frame.getFirstRollScore());
         Assert.assertEquals("wrong second Roll score", 0, frame.getSecondRollScore());
         Assert.assertEquals("wrong third Roll score", 0, frame.getThirdRollScore());
+    }
+
+    /**
+     * Test for simple roll without strike and spare,
+     * just test currentRoll parameter, first and second roll score parameters,
+     * and total Frame Score
+     */
+    @Test
+    public void testNextRoll() {
+        Assert.assertEquals(1, frame.getCurrentRoll());
+        frame.nextRoll(5);
+        Assert.assertEquals("first roll is wrong", 5, frame.getFirstRollScore());
+        Assert.assertEquals("current roll is wrong", 2, frame.getCurrentRoll());
+        frame.nextRoll(4);
+        Assert.assertEquals("second roll score is wrong" , 4, frame.getSecondRollScore());
+        Assert.assertEquals("frame total score is wrong", 9, frame.getTotalFrameScore());
+        Assert.assertFalse("total frame score is bigger than 10", frame.getTotalFrameScore() > 10);
     }
 }
