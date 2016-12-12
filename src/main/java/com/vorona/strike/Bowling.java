267fca30af5ca1cd0f37220331e23bccde4c17c9
vikvorona <vikvorona@yandex.ru>
2016-12-12 14:14:47 +0300

    third step - test spare

--- src/main/java/com/vorona/strike/Bowling.java
+++ src/main/java/com/vorona/strike/Bowling.java
@@ -2,13 +2,24 @@ package com.vorona.strike;
 
 public class Bowling {
 	
-	private int score = 0;
+	private int[] rolls = new int[21];
+	private int curRoll = 0;
 	
 	public void roll(int pin) {
-		score += pin;
+		rolls[curRoll++] = pin;
 	}
 	
 	public int getScore() {
+		int score = 0;
+		int rollIndex = 0;
+		for (int frame = 0; frame < 10; frame++) {
+			if(rolls[rollIndex] + rolls[rollIndex +1] == 10) {
+				score += rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+			} else {
+				score += rolls[rollIndex] + rolls[rollIndex + 1];
+			}
+			rollIndex += 2;	
+		}
 		return score;
 	}
 }
--- src/test/java/com/vorona/strike/BowlingTest.java
+++ src/test/java/com/vorona/strike/BowlingTest.java
@@ -6,12 +6,26 @@ import org.junit.Test;
 
 public class BowlingTest {
 
+	Bowling game = new Bowling();
+	
+	private void rollIt(int pin, int rolls) {
+		for (int i=0; i<rolls; i++) {
+			game.roll(pin);
+		}
+	}
+	
 	@Test
 	public void RollGame() {
-		Bowling game = new Bowling();
-		for (int i=0; i<20; i++) {
-			game.roll(1);
-		}
+		rollIt(1,20);
 		assertEquals(20, game.getScore());
+	}
+	
+	@Test
+	public void RollSpare() {
+		game.roll(8);
+		game.roll(2);
+		game.roll(4);
+		rollIt(0,17);
+		assertEquals(18, game.getScore());
 	}
 }
